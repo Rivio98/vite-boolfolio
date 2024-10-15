42fa079e47fe6caccf4eb921e33cbbffb36a4524
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-15 15:42:23 +0200

    [UPD]Add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script>
 import AppProject from './components/AppProject.vue';
+import AppHeader from './components/AppHeader.vue';
 export default {
   components: {
     AppProject,
+    AppHeader
   }
 }
 </script>
 <template>
   <div class='text-center p-2'>
+    <AppHeader />
     <AppProject />
   </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <header>
+        <div class="container-fluid my-3">
+            <div class="row">
+                <div class="col-6">
+                    <img src="" alt="LOGO">
+                </div>
+                <div class="col-6">
+                    <ul class="list-unstyled d-flex justify-content-center">
+                        <li class="mx-2">
+                            <a href="#">Homepage</a>
+                        </li>
+                        <li class="mx-2">
+                            <a href="#">MyProjects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </header>
+</template>
+
+<style lang="">
+
+</style>
