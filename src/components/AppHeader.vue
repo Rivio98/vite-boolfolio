e9a317bb00af02353e441cf4b023043854d98778
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-15 17:28:02 +0200

    [UPD]Add correct route view and exerercise finished

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,12 +10,12 @@ export default {
         <div class="container-fluid my-3">
             <div class="row">
                 <div class="col-6">
-                    <img src="" alt="LOGO">
+                    <img src="../../public/logo.png" style="width: 100px; height: 100px;" alt="LOGO">
                 </div>
                 <div class="col-6">
                     <ul class="list-unstyled d-flex justify-content-center">
                         <li class="mx-2" v-for="item, index in menu" :key="index">
-                            <a href="#">{{ item.label }}</a>
+                            <router-link :to="{ name: item.name }">{{ item.label }}</router-link>
                         </li>
                     </ul>
                 </div>
